f44b47434bd961d94e99aa8f11ec1fa96698efc0
Joseph Sesti <sestijoey63@gmail.com>
2019-09-11 19:08:55 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,43 @@
+class TicTacToe
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [6,4,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+    ]
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(user_input)
+    user_input.to_i-1
+  end
+  
+  def move(index, user_input)
+    user_input = board[index]
+    user_input.to_index
+  end
+  
+  def position_taken?(board, i)
+    if (board[i] == " ") || (board[i] == "") || (board[i] == nil)
+      return false
+    else
+      return true
+    end
+  end
+    
+end
